5936959b0ee46201cfab8b8676a66aaf26878842
meganeroppy <roppy@hotmail.co.jp>
2018-05-29 17:50:10 +0900

    cmt

---
+++ Assets/OpenCVForUnity/Examples/ContribModules/aruco/ArUcoExample/ARUtils.cs
@@ -0,0 +1,190 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace OpenCVForUnityExample
+{
+    /// <summary>
+    /// AR utils.
+    /// </summary>
+    public class ARUtils
+    {
+        /// <summary>
+        /// Extract translation from transform matrix.
+        /// </summary>
+        /// <param name="matrix">Transform matrix. This parameter is passed by reference
+        /// to improve performance; no changes will be made to it.</param>
+        /// <returns>
+        /// Translation offset.
+        /// </returns>
+        public static Vector3 ExtractTranslationFromMatrix (ref Matrix4x4 matrix)
+        {
+            Vector3 translate;
+            translate.x = matrix.m03;
+            translate.y = matrix.m13;
+            translate.z = matrix.m23;
+            return translate;
+        }
+        
+        /// <summary>
+        /// Extract rotation quaternion from transform matrix.
+        /// </summary>
+        /// <param name="matrix">Transform matrix. This parameter is passed by reference
+        /// to improve performance; no changes will be made to it.</param>
+        /// <returns>
+        /// Quaternion representation of rotation transform.
+        /// </returns>
+        public static Quaternion ExtractRotationFromMatrix (ref Matrix4x4 matrix)
+        {
+            Vector3 forward;
+            forward.x = matrix.m02;
+            forward.y = matrix.m12;
+            forward.z = matrix.m22;
+            
+            Vector3 upwards;
+            upwards.x = matrix.m01;
+            upwards.y = matrix.m11;
+            upwards.z = matrix.m21;
+            
+            return Quaternion.LookRotation (forward, upwards);
+        }
+        
+        /// <summary>
+        /// Extract scale from transform matrix.
+        /// </summary>
+        /// <param name="matrix">Transform matrix. This parameter is passed by reference
+        /// to improve performance; no changes will be made to it.</param>
+        /// <returns>
+        /// Scale vector.
+        /// </returns>
+        public static Vector3 ExtractScaleFromMatrix (ref Matrix4x4 matrix)
+        {
+            Vector3 scale;
+            scale.x = new Vector4 (matrix.m00, matrix.m10, matrix.m20, matrix.m30).magnitude;
+            scale.y = new Vector4 (matrix.m01, matrix.m11, matrix.m21, matrix.m31).magnitude;
+            scale.z = new Vector4 (matrix.m02, matrix.m12, matrix.m22, matrix.m32).magnitude;
+            return scale;
+        }
+        
+        /// <summary>
+        /// Extract position, rotation and scale from TRS matrix.
+        /// </summary>
+        /// <param name="matrix">Transform matrix. This parameter is passed by reference
+        /// to improve performance; no changes will be made to it.</param>
+        /// <param name="localPosition">Output position.</param>
+        /// <param name="localRotation">Output rotation.</param>
+        /// <param name="localScale">Output scale.</param>
+        public static void DecomposeMatrix (ref Matrix4x4 matrix, out Vector3 localPosition, out Quaternion localRotation, out Vector3 localScale)
+        {
+            localPosition = ExtractTranslationFromMatrix (ref matrix);
+            localRotation = ExtractRotationFromMatrix (ref matrix);
+            localScale = ExtractScaleFromMatrix (ref matrix);
+        }
+        
+        /// <summary>
+        /// Set transform component from TRS matrix.
+        /// </summary>
+        /// <param name="transform">Transform component.</param>
+        /// <param name="matrix">Transform matrix. This parameter is passed by reference
+        /// to improve performance; no changes will be made to it.</param>
+        public static void SetTransformFromMatrix (Transform transform, ref Matrix4x4 matrix)
+        {
+            transform.localPosition = ExtractTranslationFromMatrix (ref matrix);
+            transform.localRotation = ExtractRotationFromMatrix (ref matrix);
+            transform.localScale = ExtractScaleFromMatrix (ref matrix);
+        }
+
+        /// <summary>
+        /// Calculate projection matrix from camera matrix values.
+        /// </summary>
+        /// <param name="fx">Focal length x.</param>
+        /// <param name="fy">Focal length y.</param>
+        /// <param name="cx">Image center point x.(principal point x)</param>
+        /// <param name="cy">Image center point y.(principal point y)</param>
+        /// <param name="width">Image width.</param>
+        /// <param name="height">Image height.</param>
+        /// <param name="near">The near clipping plane distance.</param>
+        /// <param name="far">The far clipping plane distance.</param>
+        /// <returns>
+        /// Projection matrix.
+        /// </returns>
+        public static Matrix4x4 CalculateProjectionMatrixFromCameraMatrixValues (float fx, float fy, float cx, float cy, float width, float height, float near, float far)
+        {
+            Matrix4x4 projectionMatrix = new Matrix4x4 ();
+            projectionMatrix.m00 = 2.0f * fx / width;
+            projectionMatrix.m02 = 1.0f - 2.0f * cx / width;
+            projectionMatrix.m11 = 2.0f * fy / height;
+            projectionMatrix.m12 = - 1.0f + 2.0f * cy / height;
+            projectionMatrix.m22 = -(far + near) / (far - near);
+            projectionMatrix.m23 = -2.0f * far * near / (far - near);
+            projectionMatrix.m32 = -1.0f;
+
+            return projectionMatrix;
+        }
+
+        /// <summary>
+        /// Calculate camera matrix values from projection matrix.
+        /// </summary>
+        /// <param name="projectionMatrix">Projection matrix.</param>
+        /// <param name="width">Image width.</param>
+        /// <param name="height">Image height.</param>
+        /// <param name="fovV">Vertical field of view.</param>
+        /// <returns>
+        /// Camera matrix values. (fx = matrx.m00, fy = matrx.m11, cx = matrx.m02, cy = matrx.m12)
+        /// </returns>
+        public static Matrix4x4 CameraMatrixValuesFromCalculateProjectionMatrix (Matrix4x4 projectionMatrix, float width, float height, float fovV)
+        {
+            float fovH = 2.0f * Mathf.Atan (width/height * Mathf.Tan (fovV*Mathf.Deg2Rad / 2.0f)) * Mathf.Rad2Deg;
+
+            Matrix4x4 cameraMatrix = new Matrix4x4 ();
+            cameraMatrix.m00 = CalculateDistance (width, fovH);
+            cameraMatrix.m02 = -((projectionMatrix.m02*width - width) / 2);
+            cameraMatrix.m11 = CalculateDistance (height, fovV);
+            cameraMatrix.m12 = (projectionMatrix.m12*height + height) / 2;
+            cameraMatrix.m22 = 1.0f;
+
+            return cameraMatrix;
+        }
+
+        /// <summary>
+        /// Calculate frustum size.
+        /// https://docs.unity3d.com/Manual/FrustumSizeAtDistance.html
+        /// </summary>
+        /// <param name="distance">Distance.</param>
+        /// <param name="fov">Field of view. (horizontal or vertical direction)</param>
+        /// <returns>
+        /// Frustum height.
+        /// </returns>
+        public static float CalculateFrustumSize (float distance, float fov)
+        {
+            return 2.0f * distance * Mathf.Tan(fov * 0.5f * Mathf.Deg2Rad);
+        }
+
+        /// <summary>
+        /// Calculate distance.
+        /// https://docs.unity3d.com/Manual/FrustumSizeAtDistance.html
+        /// </summary>
+        /// <param name="frustumHeight">One side size of a frustum.</param>
+        /// <param name="fov">Field of view. (horizontal or vertical direction)</param>
+        /// <returns>
+        /// Distance.
+        /// </returns>
+        public static float CalculateDistance (float frustumSize, float fov)
+        {
+            return frustumSize * 0.5f / Mathf.Tan(fov * 0.5f * Mathf.Deg2Rad);
+        }
+
+        /// <summary>
+        /// Calculate FOV angle.
+        /// https://docs.unity3d.com/Manual/FrustumSizeAtDistance.html
+        /// </summary>
+        /// <param name="frustumHeight">One side size of a frustum.</param>
+        /// <param name="distance">Distance.</param>
+        /// <returns>
+        /// FOV angle.
+        /// </returns>
+        public static float CalculateFOVAngle (float frustumSize, float distance)
+        {
+            return 2.0f * Mathf.Atan (frustumSize * 0.5f / distance) * Mathf.Rad2Deg;
+        }
+    }
+}
---
+++ Assets/OpenCVForUnity/Examples/MainModules/videoio/VideoCaptureExample/VideoCaptureExample.cs
@@ -0,0 +1,211 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+#if UNITY_5_3 || UNITY_5_3_OR_NEWER
+using UnityEngine.SceneManagement;
+#endif
+using OpenCVForUnity;
+
+namespace OpenCVForUnityExample
+{
+    /// <summary>
+    /// VideoCapture Example
+    /// An example of playing a video file using the VideoCapture class.
+    /// http://docs.opencv.org/3.2.0/dd/d43/tutorial_py_video_display.html
+    /// </summary>
+    public class VideoCaptureExample : MonoBehaviour
+    {
+        /// <summary>
+        /// The videocapture.
+        /// </summary>
+        VideoCapture capture;
+
+        /// <summary>
+        /// The rgb mat.
+        /// </summary>
+        Mat rgbMat;
+
+        /// <summary>
+        /// The colors.
+        /// </summary>
+        Color32[] colors;
+
+        /// <summary>
+        /// The texture.
+        /// </summary>
+        Texture2D texture;
+
+        /// <summary>
+        /// Indicates whether the video frame needs updating.
+        /// </summary>
+        bool shouldUpdateVideoFrame = false;
+
+        /// <summary>
+        /// The prev frame tick count.
+        /// </summary>
+        long prevFrameTickCount;
+
+        /// <summary>
+        /// The current frame tick count.
+        /// </summary>
+        long currentFrameTickCount;
+
+        #if UNITY_WEBGL && !UNITY_EDITOR
+        Stack<IEnumerator> coroutines = new Stack<IEnumerator> ();
+        #endif
+        
+        // Use this for initialization
+        void Start ()
+        {
+            capture = new VideoCapture ();
+
+            #if UNITY_WEBGL && !UNITY_EDITOR
+            var getFilePath_Coroutine = Utils.getFilePathAsync("768x576_mjpeg.mjpeg", (result) => {
+                coroutines.Clear ();
+
+                capture.open (result);
+                Initialize ();
+            });
+            coroutines.Push (getFilePath_Coroutine);
+            StartCoroutine (getFilePath_Coroutine);
+            #else
+            capture.open (Utils.getFilePath ("768x576_mjpeg.mjpeg"));
+
+            Initialize ();
+            #endif
+        }
+
+        private void Initialize ()
+        {
+            rgbMat = new Mat ();
+
+            if (!capture.isOpened ()) {
+                Debug.LogError ("capture.isOpened() is false. Please copy from “OpenCVForUnity/StreamingAssets/” to “Assets/StreamingAssets/” folder. ");
+            }
+
+            Debug.Log ("CAP_PROP_FORMAT: " + capture.get (Videoio.CAP_PROP_FORMAT));
+            Debug.Log ("CV_CAP_PROP_PREVIEW_FORMAT: " + capture.get (Videoio.CV_CAP_PROP_PREVIEW_FORMAT));
+            Debug.Log ("CAP_PROP_POS_MSEC: " + capture.get (Videoio.CAP_PROP_POS_MSEC));
+            Debug.Log ("CAP_PROP_POS_FRAMES: " + capture.get (Videoio.CAP_PROP_POS_FRAMES));
+            Debug.Log ("CAP_PROP_POS_AVI_RATIO: " + capture.get (Videoio.CAP_PROP_POS_AVI_RATIO));
+            Debug.Log ("CAP_PROP_FRAME_COUNT: " + capture.get (Videoio.CAP_PROP_FRAME_COUNT));
+            Debug.Log ("CAP_PROP_FPS: " + capture.get (Videoio.CAP_PROP_FPS));
+            Debug.Log ("CAP_PROP_FRAME_WIDTH: " + capture.get (Videoio.CAP_PROP_FRAME_WIDTH));
+            Debug.Log ("CAP_PROP_FRAME_HEIGHT: " + capture.get (Videoio.CAP_PROP_FRAME_HEIGHT));
+            double ext = capture.get (Videoio.CAP_PROP_FOURCC);
+            Debug.Log ("CAP_PROP_FOURCC: " + (char)((int)ext & 0XFF) + (char)(((int)ext & 0XFF00) >> 8) + (char)(((int)ext & 0XFF0000) >> 16) + (char)(((int)ext & 0XFF000000) >> 24));
+
+            capture.grab ();
+            capture.retrieve (rgbMat, 0);
+            int frameWidth = rgbMat.cols ();
+            int frameHeight = rgbMat.rows ();
+            colors = new Color32[frameWidth * frameHeight];
+            texture = new Texture2D (frameWidth, frameHeight, TextureFormat.RGB24, false);
+            gameObject.transform.localScale = new Vector3 ((float)frameWidth, (float)frameHeight, 1);
+            float widthScale = (float)Screen.width / (float)frameWidth;
+            float heightScale = (float)Screen.height / (float)frameHeight;
+            if (widthScale < heightScale) {
+                Camera.main.orthographicSize = ((float)frameWidth * (float)Screen.height / (float)Screen.width) / 2;
+            } else {
+                Camera.main.orthographicSize = (float)frameHeight / 2;
+            }
+            capture.set (Videoio.CAP_PROP_POS_FRAMES, 0);
+
+            gameObject.GetComponent<Renderer> ().material.mainTexture = texture;
+
+            StartCoroutine ("WaitFrameTime");
+        }
+
+        // Update is called once per frame
+        void Update ()
+        {            
+            if (shouldUpdateVideoFrame) {
+                shouldUpdateVideoFrame = false;
+
+                //Loop play
+                if (capture.get (Videoio.CAP_PROP_POS_FRAMES) >= capture.get (Videoio.CAP_PROP_FRAME_COUNT))
+                    capture.set (Videoio.CAP_PROP_POS_FRAMES, 0);
+
+                if (capture.grab ()) {
+
+                    capture.retrieve (rgbMat, 0);
+
+                    Imgproc.cvtColor (rgbMat, rgbMat, Imgproc.COLOR_BGR2RGB);
+
+                    int ff = Core.FONT_HERSHEY_SIMPLEX;
+                    double fs = 0.4;
+                    Scalar c = new Scalar (255, 255, 255);
+                    int t = 0;
+                    int lt = Imgproc.LINE_AA;
+                    bool blo = false;
+                    Imgproc.putText (rgbMat, "CAP_PROP_FORMAT: " + capture.get (Videoio.CAP_PROP_FORMAT), new Point (rgbMat.cols() - 300, 20), ff, fs, c, t, lt, blo);
+                    Imgproc.putText (rgbMat, "CV_CAP_PROP_PREVIEW_FORMAT: " + capture.get (Videoio.CV_CAP_PROP_PREVIEW_FORMAT), new Point (rgbMat.cols() - 300, 40), ff, fs, c, t, lt, blo);
+                    Imgproc.putText (rgbMat, "CAP_PROP_POS_MSEC: " + capture.get (Videoio.CAP_PROP_POS_MSEC), new Point (rgbMat.cols() - 300, 60), ff, fs, c, t, lt, blo);
+                    Imgproc.putText (rgbMat, "CAP_PROP_POS_FRAMES: " + capture.get (Videoio.CAP_PROP_POS_FRAMES), new Point (rgbMat.cols() - 300, 80), ff, fs, c, t, lt, blo);
+                    Imgproc.putText (rgbMat, "CAP_PROP_POS_AVI_RATIO: " + capture.get (Videoio.CAP_PROP_POS_AVI_RATIO), new Point (rgbMat.cols() - 300, 100), ff, fs, c, t, lt, blo);
+                    Imgproc.putText (rgbMat, "CAP_PROP_FRAME_COUNT: " + capture.get (Videoio.CAP_PROP_FRAME_COUNT), new Point (rgbMat.cols() - 300, 120), ff, fs, c, t, lt, blo);
+                    Imgproc.putText (rgbMat, "CAP_PROP_FPS: " + capture.get (Videoio.CAP_PROP_FPS), new Point (rgbMat.cols() - 300, 140), ff, fs, c, t, lt, blo);
+                    Imgproc.putText (rgbMat, "CAP_PROP_FRAME_WIDTH: " + capture.get (Videoio.CAP_PROP_FRAME_WIDTH), new Point (rgbMat.cols() - 300, 160), ff, fs, c, t, lt, blo);
+                    Imgproc.putText (rgbMat, "CAP_PROP_FRAME_HEIGHT: " + capture.get (Videoio.CAP_PROP_FRAME_HEIGHT), new Point (rgbMat.cols() - 300, 180), ff, fs, c, t, lt, blo);
+                    double ext = capture.get (Videoio.CAP_PROP_FOURCC);
+                    Imgproc.putText (rgbMat, "CAP_PROP_FOURCC: " + (char)((int)ext & 0XFF) + (char)(((int)ext & 0XFF00) >> 8) + (char)(((int)ext & 0XFF0000) >> 16) + (char)(((int)ext & 0XFF000000) >> 24), new Point (rgbMat.cols() - 300, 200), ff, fs, c, t, lt, blo);
+
+                    int msec = (int)Math.Round(1000.0 * (currentFrameTickCount - prevFrameTickCount) / Core.getTickFrequency ());
+                    int fps = (int)Math.Round(1000.0 / msec);
+                    Imgproc.putText (rgbMat, "STATE: " + msec + "ms " + " (" + fps + "fps)", new Point (rgbMat.cols() - 300, 240), ff, fs, c, t, lt, blo);
+                
+                    Utils.matToTexture2D (rgbMat, texture, colors);
+                }
+            }
+        }
+
+        private IEnumerator WaitFrameTime()
+        {
+            double videoFPS = (capture.get (Videoio.CAP_PROP_FPS) <= 0) ? 10.0 : capture.get (Videoio.CAP_PROP_FPS);
+            int frameTime_msec = (int)Math.Round(1000.0 / videoFPS);
+
+            while (true) {
+                shouldUpdateVideoFrame = true;
+
+                prevFrameTickCount = currentFrameTickCount;
+                currentFrameTickCount = Core.getTickCount ();
+
+                yield return new WaitForSeconds (frameTime_msec / 1000f);
+            }
+        }
+
+        /// <summary>
+        /// Raises the destroy event.
+        /// </summary>
+        void OnDestroy ()
+        {
+            StopCoroutine ("WaitFrameTime");
+
+            capture.release ();
+
+            if (rgbMat != null)
+                rgbMat.Dispose ();
+
+            #if UNITY_WEBGL && !UNITY_EDITOR
+            foreach (var coroutine in coroutines) {
+                StopCoroutine (coroutine);
+                ((IDisposable)coroutine).Dispose ();
+            }
+            #endif
+        }
+
+        /// <summary>
+        /// Raises the back button click event.
+        /// </summary>
+        public void OnBackButtonClick ()
+        {
+            #if UNITY_5_3 || UNITY_5_3_OR_NEWER
+            SceneManager.LoadScene ("OpenCVForUnityExample");
+            #else
+            Application.LoadLevel ("OpenCVForUnityExample");
+            #endif
+        }
+    }
+}
