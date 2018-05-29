b5243fe5e1a73d474f07098d71ad77fed62eb2fa
meganeroppy <roppy@hotmail.co.jp>
2018-05-29 17:46:16 +0900

    cmt

---
+++ Assets/SetMeshRendererSortingLayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetMeshRendererSortingLayer : MonoBehaviour
+{
+	[SerializeField]
+	private Renderer myRenderer;
+
+	[SerializeField]
+	private string sortingLayerName;
+
+	[SerializeField]
+	private int sortingOrder;
+
+	public void Start()
+	{
+		myRenderer.sortingLayerName = sortingLayerName;
+		myRenderer.sortingOrder = sortingOrder;
+	}
+}
