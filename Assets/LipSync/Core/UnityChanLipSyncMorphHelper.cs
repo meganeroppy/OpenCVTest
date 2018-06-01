4abe5fb6d578d2995c6eb4a191ecc3629bc00107
meganeroppy <roppy@hotmail.co.jp>
2018-06-01 15:07:21 +0900

    cmt

---
+++ Assets/LipSync/Core/UnityChanLipSyncMorphHelper.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Linq;
+
+[RequireComponent (typeof(UnityChanMouseMorph))]
+public class UnityChanLipSyncMorphHelper : MonoBehaviour
+{
+    public string morphName   = "";
+    public float  morphSpeed  = 1f;
+	public float  morphWeight = 0f;
+
+	private float morphStep {
+		get { return Time.deltaTime / morphSpeed; }
+	}
+
+	private UnityChanMouseMorph.Morph morph_;
+
+	void Start()
+	{
+		morph_ = GetComponent<UnityChanMouseMorph>().morphs.First(morph => {
+			return morph.name == morphName;
+		});
+	}
+
+	void Update()
+	{
+		var deltaMorph = morphWeight - morph_.weight;
+		if (Mathf.Abs(deltaMorph) > morphStep) {
+			morph_.weight += ( (deltaMorph > 0) ? 1f : -1f ) * morphStep;
+		} else {
+			morph_.weight += deltaMorph * 0.5f;
+		}
+	}
+}
