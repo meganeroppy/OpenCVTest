8b89cac82650c8047c8b1bc64dbd217ad1432af3
meganeroppy <roppy@hotmail.co.jp>
2018-06-01 15:07:02 +0900

    cmt

---
+++ Assets/Script/UnitychanVoicePlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent( typeof(UnityChanLipSync) )]
+public class UnityChanVoicePlayer : MonoBehaviour 
+{
+	private UnityChanLipSync lipSync_;
+	public AudioClip[] audioClips;
+	public int index = 0;
+
+	void Start()
+	{
+		lipSync_ = GetComponent<UnityChanLipSync>();
+	}
+
+	void Update() 
+	{
+		if (Input.anyKeyDown) {
+			if (index < 0 || index >= audioClips.Length) index = 0; 
+			lipSync_.Play( audioClips[index] );
+			++index;
+		}
+	}
+}
