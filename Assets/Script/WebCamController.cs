53144c64a8877035b23db7b92cb6af95524ba3ee
syuukouha <252747500@qq.com>
2018-06-10 17:46:13 +0900

    WEBカメラの映像を映すスクリプト追加（顔認識とかはしない） インスペクタで機器インデックスを指定する

---
+++ Assets/Script/WebCamController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// カメラの映像を映す
+/// 顔認識とかはしない
+/// </summary>
+public class WebCamController : MonoBehaviour
+{
+	int width = 1920;
+	int height = 1080;
+	int fps = 30;
+	WebCamTexture webcamTexture;
+
+    [SerializeField]
+    int cameraIndex = 0;
+	void Start () {
+		WebCamDevice[] devices = WebCamTexture.devices;
+        cameraIndex = cameraIndex % devices.Length;
+
+		webcamTexture = new WebCamTexture(devices[cameraIndex].name, this.width, this.height, this.fps);
+		GetComponent<Renderer> ().material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+	}
+}
