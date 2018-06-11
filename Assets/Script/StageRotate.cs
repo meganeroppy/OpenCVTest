2b492febb68e3260cd8a8d85dd76dacee744a2f1
syuukouha <252747500@qq.com>
2018-06-11 21:41:53 +0900

    左右キーで中の人の景色を回転

---
+++ Assets/Script/StageRotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageRotate : MonoBehaviour {
+
+    [SerializeField]
+    float rotSpeed;
+
+
+    // Update is called once per frame
+    void Update() {
+        var h = Input.GetAxis("Horizontal");
+
+        transform.Rotate(Vector3.up * -h * rotSpeed * Time.deltaTime);
+	}
+}
