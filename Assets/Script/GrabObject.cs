4b2b66f814ffc8344ac714ae655e3100a8263b57
syuukouha <252747500@qq.com>
2018-08-02 18:06:42 +0900

    コメントをつかむ処理仮実装

---
+++ Assets/Script/GrabObject.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof( SteamVR_TrackedObject))]
+public class GrabObject : MonoBehaviour {
+
+    GameObject current = null;
+
+    SteamVR_TrackedObject controller;
+
+    bool grabbedPrev = false;
+
+    [SerializeField]
+    float grabDistance = 0.1f;
+
+    [SerializeField]
+    Transform originParent;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var device = SteamVR_Controller.Input((int)controller.index);
+        if (device == null) return;
+
+//        var triggered = device.GetPress(SteamVR_Controller.ButtonMask.Trigger);
+//        var gripped = device.GetPress(SteamVR_Controller.ButtonMask.Grip);
+        var grabbed = device.GetPress(SteamVR_Controller.ButtonMask.Trigger) || device.GetPress(SteamVR_Controller.ButtonMask.Grip);
+
+        // 握ってない -> 握る
+        if( !grabbedPrev && grabbed )
+        {
+            Grab();
+        }
+
+        // 握る -> 握ってない
+        if (grabbedPrev && !grabbed)
+        {
+            Release();
+        }
+
+        grabbedPrev = grabbed;
+    }
+
+    void Grab()
+    {
+        if (current != null) return;
+
+        var pool = CommentBoard.pool;
+        float nearest = float.MaxValue;
+
+        GameObject target = null;
+        for( int i=0; i<pool.Count; ++i )
+        {
+            var obj = pool[i];
+            if (!obj.gameObject.activeInHierarchy) continue;
+
+            var distance = (transform.position - obj.transform.position).magnitude;
+            if ( distance < grabDistance )
+            {
+                if( distance < nearest )
+                {
+                    target = obj.gameObject;
+                    nearest = distance;
+                }
+            }
+        }
+
+        if (target == null) return;
+
+        current = target;
+
+        current.transform.SetParent(transform);
+    }
+
+    void Release()
+    {
+        if (current == null) return;
+
+        current.transform.SetParent(originParent);
+
+        current = null;
+    }
+}
