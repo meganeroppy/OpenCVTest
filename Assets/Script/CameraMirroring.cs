669d0a68f7a25f3e3d720fec1c9689cb570b32f7
syuukouha <252747500@qq.com>
2018-08-02 14:04:12 +0900

    ディレクトリ整理など

---
+++ Assets/Script/CameraMirroring.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraMirroring : MonoBehaviour {
+
+    [SerializeField]
+    RawImage rawImage = null;
+
+    [SerializeField]
+    Camera[] cameras = null;
+
+    int currentIndex = 0;
+
+	// Update is called once per frame
+	void Update () {
+
+        for (int i = 0; i < cameras.Length; ++i)
+        {
+            rawImage.texture = cameras[EggGameManager.instance.CurrentCameraIndex].targetTexture;
+        }
+    }
+}
